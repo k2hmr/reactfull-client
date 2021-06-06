c92194329524f3a93d1fbeb63aa018f722b5478d
kajimura <k2hmr94@gmail.com>
2021-06-06 16:59:09 +0900

    ブラウザのコンソールでlocalStrege.setItem("accessToken", "...")でログインできないようにする、フロント側からの認証機能を追加 ログインしたらログインボタンが消える機能を追加

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const {setAuthState} = useContext(AuthContext);
 
   let history = useHistory();
 
@@ -17,7 +19,8 @@ function Login() {
       }
       else
       {
-        sessionStorage.setItem("accessToken", response.data);
+        localStorage.setItem("accessToken", response.data);
+        setAuthState(true);
         history.push("/");
       }
     });
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -26,14 +26,17 @@ function Post() {
       },
       {
         headers: {
-          accessToken: sessionStorage.getItem("accessToken"),
+          accessToken: localStorage.getItem("accessToken"),
         },
       }
       ).then((response) => {
         if(response.data.error){
           alert(response.data.error);
         }else{
-          const commentToAdd = { commentBody: newComment };
+          const commentToAdd = {
+            commentBody: newComment,
+            username: response.data.username,
+          };
           setComments([...comments, commentToAdd]);
           setNewComment("");
         }
@@ -64,7 +67,10 @@ function Post() {
         </div>
         <div className="listOfComments">
           {comments.map((comment, key) => {
-            return <div key={key} className="comment"> {comment.commentBody} </div>
+            return <div key={key} className="comment">
+              {comment.commentBody}
+              <label>Username: {comment.username}</label>
+            </div>
           })}
         </div>
       </div>
