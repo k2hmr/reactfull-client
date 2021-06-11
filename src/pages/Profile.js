f986d514013c9421bc1baaaea53d5683de33c25f
kajimura <k2hmr94@gmail.com>
2021-06-11 23:45:36 +0900

    投稿のタイトルと投稿内容の変更ができるようにした。パスワードの変更ができるようにした。

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Profile() {
   let { id } = useParams();
   let history = useHistory();
   const [username, setUsername] = useState("");
   const [listOfPosts, setListOfPosts] = useState([]);
+  const { authState } = useContext(AuthContext);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
@@ -21,6 +23,13 @@ function Profile() {
     <div className="profilePageContainer">
       <div className="basicInfo">
         <h1> Username: {username}</h1>
+        {authState.username === username && (
+          <button
+            onClick={() => {
+              history.push("/changepassword");
+            }}
+          > Change My Password</button>
+        )}
       </div>
       <div className="listOfPosts">
         {listOfPosts.map((value, key) => {
