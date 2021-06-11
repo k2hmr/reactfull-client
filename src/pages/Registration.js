e7ca996322f862bed8911e2388d665d38e1a1987
kajimura <k2hmr94@gmail.com>
2021-06-12 02:42:10 +0900

    Changes

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -16,7 +16,7 @@ function Registration() {
   });
 
   const onSubmit = (data) => {
-    axios.post("http://localhost:3001/auth", data).then(() => {
+    axios.post("https://full-stack-api-k2hmr.herokuapp.com/auth", data).then(() => {
       console.log(data);
     });
   };
